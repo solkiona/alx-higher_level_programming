ca62797f240a864cfef21f401653a3f0a63a0214
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-06-13 07:43:34 -0700

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,14 @@ number = random.randint(-10000, 10000)
 
 if number > 0:
     l_digit = number % 10
-elif number < 0:
+else:
     l_digit = number % -10
-print(f"Last digit of {number:d} is {l_digit:d}", end = '')
+
+print(f"Last digit of {number:d} is {l_digit:d}", end='')
+
 if l_digit > 5:
     print(" and is greater than 5")
 elif l_digit == 0:
     print(" and is 0")
-elif l_digit < 6 and not 0:
+else:
     print(" and is less than 6 and not 0")
