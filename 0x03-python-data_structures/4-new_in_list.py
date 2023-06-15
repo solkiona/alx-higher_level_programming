b01b35ca7ce837c7ff1ea8535c9e24b1b8c88931
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-06-15 05:33:44 -0700

    print matrix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cp_list = my_list.copy()
-    cp_list[idx] = element
+    length = len(my_list)
 
-    if idx < 0:
-        return cp_list
-    if idx >= len(my_list):
-        return cp_list
-    else:
-        return cp_list
+    new_list = my_list[:]
+
+    if 0 <= idx < length:
+        new_list[idx] = element
+
+    return (new_list)
