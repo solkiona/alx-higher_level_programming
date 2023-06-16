15840735743d1b58037ead0d9106557b7b89b462
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-06-16 05:13:55 -0700

    reading commandline arguments in python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     import sys
     i = len(sys.argv) - 1
-    
+
     if i == 0:
         print("{} arguments.".format(i))
     elif i == 1:
-        print("{} argument.".format(i))
+        print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
 
