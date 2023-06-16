6d613a8d41970b2f1437865fed7be2a27014d7fa
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-06-16 05:12:03 -0700

    reading commandline arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,9 +11,8 @@ if __name__ == "__main__":
         print("{} arguments:".format(i))
 
     if i >= 1:
-        j = 0
+        i = 0
         for arg in sys.argv:
-            if j != 0:
-                print("{}: {}".format(j, arg))
-            j += 1
-
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
