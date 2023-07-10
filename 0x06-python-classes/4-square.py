3613dff895dc4f8daffce6d6325619523a3af84a
Adule Solomon Onwukwe <solkiona@gmail.com>
2023-07-10 09:37:34 -0700

    getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module defines  an Area of a Square
+"""
+class Square:
+    """This class defines the Area of a Square
+
+    Attributes:
+        size (int): Size of the Square
+    """
+    def __init__(self, size=0):
+        """This method initializes the attribute size
+
+        Attributes:
+            size (int): Size of the Square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ Method to return the size value
+        """
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """ Method to set the size value of the square object
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
